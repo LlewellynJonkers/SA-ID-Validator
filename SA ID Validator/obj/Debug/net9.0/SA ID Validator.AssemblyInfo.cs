7059f1806e457f9f09228c67ab2a8ea73e0370df
LlewellynJonkers <l.jonkers220@gmail.com>
2026-03-24 11:05:53 +0200

    Fixed some javascript errors

--- SA ID Validator/obj/Debug/net9.0/SA ID Validator.AssemblyInfo.cs
+++ SA ID Validator/obj/Debug/net9.0/SA ID Validator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SA ID Validator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48017c0f2cbb67f1db8611a7af33e87cbc42c46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1230f3e2615ab5298f1f4ac907cdd631907fbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SA ID Validator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SA ID Validator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
